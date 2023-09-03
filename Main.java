b7022f0b92a07c0392703b77a43d56f087eae181
marianazevallos <142361942+marianazevallos@users.noreply.github.com>
2023-09-02 22:02:24 -0500

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,35 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+public class Main
+{
+	public static void main(String[] args) 
+	
+	{
+	    //ejercicio a
+	    if (6<6*5)
+		System.out.println("Hello");
+		System.out.println(" There");
+		
+		//respuesta: Hello
+		// There
+		
+		/* ejercicio b */ 
+		
+		int x=1, y=2, z=3;
+		
+		if(x>y)
+	    	if(x>z)
+		    System.out.println("1111");
+	    	else
+		    System.out.println("2222");
+	
+		//Respueta: no imprime nada
+
+	}
+}
